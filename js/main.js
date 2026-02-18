cc35cfcde7869a8fb20f47f19c1aa6d354080c8f
koosverhagen <kverhagen@mac.com>
2026-02-18 08:26:30 +0000

    Update main.js

--- js/main.js
+++ js/main.js
@@ -29,10 +29,12 @@ document.addEventListener("click", function(e) {
   const placeholder = e.target.closest(".video-placeholder");
   if (!placeholder) return;
 
-  const videoID = placeholder.dataset.video;
+  const videoID = placeholder.getAttribute("data-video");
 
   placeholder.innerHTML = `
     <iframe
+      width="100%"
+      height="100%"
       src="https://www.youtube-nocookie.com/embed/${videoID}?autoplay=1&modestbranding=1&rel=0"
       frameborder="0"
       allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
